80207706ba3aefeaa94a54598d8589bcdf6eee10
Jason Haselden <c.haselden@aol.com>
2023-08-05 10:53:16 -0400

    New AI features in progress

--- scripts.js
+++ scripts.js
@@ -1,32 +1,45 @@
-let player1, player2, currentTurn, gameActive, timerId;
+let player1, player2 = "AI", currentTurn, gameActive, timerId, aiPlayer = false, difficulty = "easy"; // Added default value for difficulty
 const winningMessageDiv = document.getElementById("winning-message");
 const timerDiv = document.getElementById("timer");
 const gameBoard = document.getElementById("game-board");
-let cells = [];  
+let cells = [];
 
 const winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Columns
-    [0, 4, 8], [2, 4, 6]               // Diagonals
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
 ];
 
 document.getElementById("start-button").addEventListener("click", startGame);
 document.getElementById("reset-button").addEventListener("click", resetGame);
+document.addEventListener("DOMContentLoaded", function() {
+    togglePlayer2Input();
+});
+
 
 function startGame() {
-    player1 = document.getElementById("player1").value;
-    player2 = document.getElementById("player2").value;
-    if (!player1 || !player2) {
-        alert("Please enter names for both players");
-        return;
+    player1 = document.getElementById("player1").value || "Player 1";  // Default to "Player 1" if no name is provided
+
+    let player2Type = document.getElementById("player2-type").value;
+    if (player2Type === "human") {
+        player2 = document.getElementById("player2-name").value || "Player 2";  // Default to "Player 2" if no name is provided
     }
+
+    aiPlayer = (player2Type === "AI");
+    difficulty = document.getElementById("difficulty").value; // Assuming you have a dropdown with id "difficulty" for AI difficulty selection
+
     currentTurn = "X";
     gameActive = true;
-    this.disabled = true;
+
+    document.getElementById("start-button").disabled = true;
     document.getElementById("reset-button").disabled = false;
     winningMessageDiv.innerText = "";
 
-    // Create the game board
+    createGameBoard();
+    startTimer();
+}
+
+function createGameBoard() {
     while (gameBoard.firstChild) {
         gameBoard.removeChild(gameBoard.firstChild);
     }
@@ -36,26 +49,22 @@ function startGame() {
         cell.addEventListener('click', handleClick, { once: true });
         gameBoard.appendChild(cell);
     }
-
     cells = Array.from(document.querySelectorAll(".cell"));
-    startTimer();
 }
 
 function resetGame() {
     cells.forEach(cell => {
         cell.innerText = '';
         cell.style.backgroundColor = "#2f3640";
-        cell.removeEventListener('click', handleClick);  
+        cell.removeEventListener('click', handleClick);
         cell.addEventListener('click', handleClick, { once: true });
     });
     document.getElementById("start-button").disabled = false;
-    this.disabled = true;
     gameActive = false;
     stopTimer();
     timerDiv.innerText = "00:00";
     winningMessageDiv.innerText = "";
 }
-
 
 function startTimer() {
     let seconds = 0;
@@ -75,48 +84,171 @@ function pad(number) {
     return number < 10 ? "0" + number : number;
 }
 
+/*function handleClick(e) {
+    const cell = e.target;
+    if (!gameActive || cell.innerText !== '') return;
+    cell.innerText = currentTurn;
+    cell.style.backgroundColor = currentTurn === "X" ? "#ff6347" : "#4682b4";
+    
+    if (checkWin()) {
+        displayWinner();
+        stopTimer();
+    } else if (isBoardFull(cells)) {
+        winningMessageDiv.innerText = "It's a Tie!";
+        gameActive = false;
+    } else {
+        currentTurn = currentTurn === "X" ? "O" : "X";
+        if (aiPlayer && currentTurn === "O") {
+            aiMove();
+        }
+    }
+}*/
+
 function handleClick(e) {
     const cell = e.target;
     if (!gameActive || cell.innerText !== '') return;
     cell.innerText = currentTurn;
+    cell.style.backgroundColor = currentTurn === "X" ? "#ff6347" : "#4682b4";
+    
     if (checkWin()) {
-        winningMessageDiv.innerText = `${currentTurn === "X" ? player1 : player2} Wins!`;
-        winningMessageDiv.style.visibility = 'visible';
-        winningMessageDiv.style.opacity = '1';
-        gameActive = false;
-        document.getElementById("reset-button").disabled = false;
+        displayWinner();
         stopTimer();
-    } else if (isTie()) {
+    } else if (isBoardFull(cells)) {
         winningMessageDiv.innerText = "It's a Tie!";
-        winningMessageDiv.style.visibility = 'visible';
-        winningMessageDiv.style.opacity = '1';
         gameActive = false;
-        document.getElementById("reset-button").disabled = false;
-        stopTimer();
     } else {
         currentTurn = currentTurn === "X" ? "O" : "X";
     }
 }
 
-function isTie() {
-    return [...cells].every(cell => cell.innerText === 'X' || cell.innerText === 'O');
+
+function isBoardFull(board) {
+    return board.every(cell => cell.innerText !== '');
 }
 
 
 function checkWin() {
     let isWinningCombination = false;
-    winningCombinations.forEach(combination => {
-        if (cells[combination[0]].innerText !== '' && cells[combination[0]].innerText === cells[combination[1]].innerText && cells[combination[1]].innerText === cells[combination[2]].innerText) {
+    for (let combination of winningCombinations) {
+        if (cells[combination[0]].innerText && cells[combination[0]].innerText === cells[combination[1]].innerText && cells[combination[1]].innerText === cells[combination[2]].innerText) {
             isWinningCombination = true;
-            combination.forEach(index => cells[index].style.backgroundColor = "green");
+            for (let index of combination) {
+                cells[index].style.backgroundColor = "green";
+            }
         }
-    });
-    if (isWinningCombination) {
-        winningMessageDiv.innerText = `${currentTurn === "X" ? player1 : player2} Wins!`;
-        winningMessageDiv.style.visibility = 'visible';
-        winningMessageDiv.style.opacity = '1';
-        gameActive = false;
-        document.getElementById("reset-button").disabled = false;
-        stopTimer();
+    }
+    return isWinningCombination;
+}
+
+function displayWinner() {
+    winningMessageDiv.innerText = `${currentTurn === "X" ? player1 : player2} Wins!`;
+    gameActive = false;
+    document.getElementById("reset-button").disabled = false;
+}
+
+
+// AI Functions
+function aiMove() {
+    if (difficulty === "easy") {
+        randomMove();
+    } else if (difficulty === "medium") {
+        if (!blockOrWin("O") && !blockOrWin("X")) {
+            randomMove();
+        }
+    } else {
+        minimaxMove();
+    }
+}
+
+function randomMove() {
+    let availableCells = cells.filter(cell => cell.innerText === '');
+    let randomCell = availableCells[Math.floor(Math.random() * availableCells.length)];
+    randomCell.click();
+}
+
+function blockOrWin(playerSign) {
+    for (let combination of winningCombinations) {
+        if (cells[combination[0]].innerText === playerSign && cells[combination[1]].innerText === playerSign && cells[combination[2]].innerText === '') {
+            cells[combination[2]].click();
+            return true;
+        } else if (cells[combination[0]].innerText === playerSign && cells[combination[2]].innerText === playerSign && cells[combination[1]].innerText === '') {
+            cells[combination[1]].click();
+            return true;
+        } else if (cells[combination[1]].innerText === playerSign && cells[combination[2]].innerText === playerSign && cells[combination[0]].innerText === '') {
+            cells[combination[0]].click();
+            return true;
+        }
+    }
+    return false;
+}
+
+// Minimax functions are implemented as previously explained
+function minimax(board, depth, isMaximizing) {
+    let winner = checkWinForMinimax(board);
+    if (winner !== null) {
+        return winner === 'X' ? -10 + depth : 10 - depth;
+    }
+
+    if (isBoardFull(board)) {
+        return 0;
+    }
+
+    let bestScore = isMaximizing ? -Infinity : Infinity;
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === '') {
+            board[i] = isMaximizing ? 'O' : 'X';
+            let score = minimax(board, depth + 1, !isMaximizing);
+            board[i] = '';  // Reset the cell
+            bestScore = isMaximizing ? Math.max(score, bestScore) : Math.min(score, bestScore);
+        }
+    }
+    return bestScore;
+}
+
+function minimaxMove() {
+    let bestScore = -Infinity;
+    let bestMove;
+
+    // Check all cells
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].innerText === '') {
+            cells[i].innerText = 'O';
+            let board = cells.map(cell => cell.innerText);
+            let score = minimax(board, 0, false);  // 0 is the initial depth, and false means next turn is of player X
+            cells[i].innerText = '';  // Reset the cell
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = i;
+            }
+        }
+    }
+
+    cells[bestMove].click();  // AI chooses the best move
+}
+
+function checkWinForMinimax(board) {
+    for (let combination of winningCombinations) {
+        if (board[combination[0]] && board[combination[0]] === board[combination[1]] && board[combination[1]] === board[combination[2]]) {
+            return board[combination[0]];
+        }
+    }
+    return null;
+}
+
+function isBoardFull(board) {
+    return board.every(cell => cell !== '');
+}
+
+function togglePlayer2Input() {
+    const playerType = document.getElementById("player2-type").value;
+    const player2NameInput = document.getElementById("player2-name");
+    const difficultyDropdown = document.getElementById("difficulty");
+
+    if (playerType === "AI") {
+        player2NameInput.style.display = 'none';
+        difficultyDropdown.style.display = 'block';
+    } else {
+        player2NameInput.style.display = 'block';
+        difficultyDropdown.style.display = 'none';
     }
 }
